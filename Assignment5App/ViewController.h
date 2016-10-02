8f9f555ff18b0a2b7b33c8b83a2bfb57ef4de945
Xander Li <x@xanderli.com>
2016-10-02 16:34:01 -0400

    Initial Commit

---
+++ Assignment5App/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Assignment5App
+//
+//  Created by Xander on 10/2/16.
+//  Copyright © 2016 Xander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Assignment5AppTests/Assignment5AppTests.m
@@ -0,0 +1,39 @@
+//
+//  Assignment5AppTests.m
+//  Assignment5AppTests
+//
+//  Created by Xander on 10/2/16.
+//  Copyright © 2016 Xander. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Assignment5AppTests : XCTestCase
+
+@end
+
+@implementation Assignment5AppTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
